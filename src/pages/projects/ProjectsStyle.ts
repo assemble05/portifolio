082a64cebb18dedc2da3dae5acc10bfb5e85a6b0
assemble05 <joneto18.jh@gmail.com>
2022-07-19 13:19:38 -0300

    feat add projects

---
+++ src/pages/projects/ProjectsStyle.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+const UlWrapper = styled.ul`
+width: 100vw;
+padding: 0;
+display: flex;
+position: absolute;
+margin-top: 60px;
+flex-direction: column;
+align-items: center;
+`
+
+export {UlWrapper}
